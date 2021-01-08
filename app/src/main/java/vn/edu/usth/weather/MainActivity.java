a7822a2f5e3f7c755553c2e08ed8010491466f72
Ntrug1 <59216905+Ntrug1@users.noreply.github.com>
2021-01-08 17:06:21 +0700

    finish practical work 11

--- app/src/main/java/vn/edu/usth/weather/MainActivity.java
+++ app/src/main/java/vn/edu/usth/weather/MainActivity.java
@@ -5,13 +5,14 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    MediaPlayer player;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,37 +26,41 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
         tabLayout.setupWithViewPager(pager);
-
-
+        player = MediaPlayer.create(this,R.raw.song);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        player.start();
         Log.i("Death", "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        player.start();
         Log.i("Death", "onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        player.pause();
         Log.i("Death", "onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        player.stop();
         Log.i("Death","onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        player.stop();
         Log.i("Death", "onDestroy");
     }
 }
