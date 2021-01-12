220dc74f4dff61f5669d8acdb9e7f77ae44719bb
Ntrug1 <59216905+Ntrug1@users.noreply.github.com>
2021-01-12 10:59:08 +0700

    practical work 13

--- app/src/main/java/vn/edu/usth/weather/MainActivity.java
+++ app/src/main/java/vn/edu/usth/weather/MainActivity.java
@@ -9,6 +9,9 @@ import androidx.viewpager.widget.ViewPager;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -38,6 +41,36 @@ public class MainActivity extends AppCompatActivity {
 //        player = MediaPlayer.create(this,R.raw.song);
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+//       create new worker thread
+        final Handler handler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+// This method is executed in main thread
+                String content = msg.getData().getString("server_response");
+                Toast.makeText(MainActivity.this, content, Toast.LENGTH_SHORT).show();
+            }
+        };
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+// this method is run in a worker thread
+                try {
+// wait for 5 seconds to simulate a long network access
+                    Thread.sleep(5000);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+// Assume that we got our data from server
+                Bundle bundle = new Bundle();
+                bundle.putString("server_response", "some sample json here");
+// notify main thread
+                Message msg = new Message();
+                msg.setData(bundle);
+                handler.sendMessage(msg);
+            }
+        });
+        t.start();
     }
 
     @Override
@@ -50,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.refresh:
-                Toast.makeText(this, "Refreshing", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(this, "Refreshing", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.menu:
                 Intent intent = new Intent(this,PrefActivity.class);
--- app/src/main/java/vn/edu/usth/weather/PrefActivity.java
+++ app/src/main/java/vn/edu/usth/weather/PrefActivity.java
@@ -1,8 +1,10 @@
 package vn.edu.usth.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class PrefActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class PrefActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pref);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
